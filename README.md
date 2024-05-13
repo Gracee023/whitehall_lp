137327806f3dde01ba6db8a3773525f4653f76a3
hardope <adeyeriopeoluwa05@gmail.com>
2024-05-13 13:21:07 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# WhiteHall LP
---
+++ main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Blog
+
+# Create your views here.
+def index(request):
+    blog = Blog.objects.all()
+    return render(request, 'index.html', context={'blogs':blog})
